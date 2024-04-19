d22ecf9fd4796598174e75ac9e3df7be28199a03
Bharath <bharath@Bharaths-MacBook-Pro.local>
2024-04-19 14:37:19 +0530

    First Commit

---
+++ src/main/java/com/bharath/trainboarding/TrainboardingApplication.java
@@ -0,0 +1,13 @@
+package com.bharath.trainboarding;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrainboardingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrainboardingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bharath/trainboarding/TrainboardingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bharath.trainboarding;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrainboardingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
